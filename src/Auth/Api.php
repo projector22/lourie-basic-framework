183ed42d71cca96b777d5bdfc7c01f7cf0f5babd
Gareth Palmer <bravdthepally@gmail.com>
2022-08-25 09:07:45 +0200

    Added methods for generating hashes:
    - generate_cookie_hash
    - generate_session_hash

--- src/Auth/Api.php
+++ src/Auth/Api.php
@@ -2,6 +2,8 @@
 
 namespace LBS\Auth;
 
+use LBS\Auth\Hash;
+
 /**
  * This class handles a number of tasks related to the API.
  * 
@@ -15,18 +17,39 @@ namespace LBS\Auth;
 class Api {
 
     /**
-     * Generate an API key. Single unique place to do this
+     * Generate an API key. Single unique place to do this.
      * 
-     * @param   string  $id     A unique identifier to encrypt.
+     * @param   string      $id         A unique identifier to encrypt.
+     * @param   string|null $random1    A random string to include in the hashing.
+     *                                  If null, a random string will be generated. 
+     *                                  Default `null`. 
+     *                                  In LRS, this should be parsed as `COOKIE_HASH`
+     * @param   string|null $random1    A random string to include in the hashing.
+     *                                  If null, a random string will be generated. 
+     *                                  Default `null`. 
+     *                                  In LRS, this should be parsed as `SESSION_HASH`
      * 
      * @return  string
      * 
      * @access  public
      * @since   3.16.1
+     * @since   3.28.0  Added param `$random1`, `$random2`
      */
 
-    public static function generate_api_key( string $id ): string {
-        return md5( COOKIE_HASH . SESSION_HASH . time() . $id );
+    public static function generate_api_key( string $id, ?string $random1 = null, ?string $random2 = null ): string {
+        return md5( 
+            $random1 ?? Hash::generate_cookie_hash(
+                Hash::random_id_string(),
+                Hash::random_id_string(),
+                Hash::random_id_string(),
+            ) . 
+            $random2 ?? Hash::generate_session_hash(
+                Hash::random_id_string(),
+                Hash::random_id_string(),
+            ) . 
+            time() . 
+            $id
+        );
     }
 
 
