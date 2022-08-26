05ba8dafb62649333439dd9b46019c53eecda2de
Gareth Palmer <bravdthepally@gmail.com>
2022-08-26 12:32:33 +0200

    Added todo

--- src/init.php
+++ src/init.php
@@ -1,2 +1,15 @@
 <?php
 
+/**
+ * @todo    Build this tool
+ * 
+ * The idea is that, but running it, it builds out the APP environment for you. This is
+ * if you are spinning up a new app from scratch. Creating files and folder permissions.
+ * 
+ * My also be used in deployment, functionally as stage one (or two after `composer install`)
+ * of an installer.
+ * 
+ * It may even be set to hook into the app's local installer.
+ * 
+ * @since   LBF 0.0.1
+ */
