224b135648df5bb77dfa3cb292987b66a451c5e5
Gareth Palmer <bravdthepally@gmail.com>
2022-09-01 07:34:17 +0200

    Added print.js #7

---
+++ src/js/print.js
@@ -0,0 +1,24 @@
+/**
+ * A set of tools for generating print and print like tasks.
+ * 
+ * @author  Gareth Palmer  [Github & Gitlab /projector22]
+ * 
+ * @requires    ES6
+ * 
+ * @since   3.13.0
+ */
+
+import URITools from './uri.js';
+
+
+/**
+ * Add a print=instruction to the url string allowing the user to generate a pdf printout
+ * 
+ * @param {string} instruction The value to assign to $_GET['print']
+ * 
+ * @since   3.6.0
+ */
+
+export function instruct_to_open_pdf(instruction) {
+    URITools.uri_change(['print', 'task'], [instruction, 'pdf']);
+}
