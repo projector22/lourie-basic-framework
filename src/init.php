b9a3dc9a940d892bd0c039299355593f28ffc093
Gareth <bravdthepally@gmail.com>
2022-10-10 12:08:37 +0200

    Added template path property #5

--- src/init.php
+++ src/init.php
@@ -16,7 +16,7 @@
  */
 
 $builder = new Init;
-$builder->gather_data();
+// $builder->gather_data();
 $builder->display_data();
 $builder->build();
 
@@ -32,7 +32,14 @@ class Init {
      * @since   LBF 0.3.0-beta
      */
 
-    private array $data;
+    // private array $data;
+    private array $data = [
+        'app_name' => 'Lourie Test App',
+        'description' => 'App for testing the init tool',
+        'author' => 'Gareth Palmer [Github & Gitlab /projector22]',
+        'version' => '0.1.0',
+        'status' => 'alpha',
+    ];
 
     /**
      * The working directory into which this tool will create all the required files.
@@ -45,6 +52,18 @@ class Init {
      */
 
     private readonly string $working_dir;
+
+    /**
+     * The directory which contains the app templates.
+     * 
+     * @var string  $template_path
+     * 
+     * @readonly
+     * @access  private
+     * @since   LBF 0.3.0-beta
+     */
+
+    private readonly string $template_path;
 
     /**
      * The directory structure used by the application.
@@ -90,19 +109,19 @@ class Init {
 
 
     const FILE_STRUCTURE = [
-        'index.php',
-        'src/router.php',
-        'src/loader.php',
-        'src/app/actions/ActionHandler.php',
-        'src/app/boilerplate/html_footer.php',
-        'src/app/boilerplate/HTMLHeader.php',
-        'src/includes/meta.php',
-        'src/includes/meta-files.php',
-        'src/includes/meta-paths.php',
-        'src/includes/meta-tables.php',
-        'src/includes/static-routes.php',
-        'src/css/styles.css',
-        'src/js/lib.js',
+        'index.php'                           => 'index.template',
+        'src/router.php'                      => '',
+        'src/loader.php'                      => '',
+        'src/app/actions/ActionHandler.php'   => '',
+        'src/app/boilerplate/html_footer.php' => '',
+        'src/app/boilerplate/HTMLHeader.php'  => '',
+        'src/includes/meta.php'               => '',
+        'src/includes/meta-files.php'         => '',
+        'src/includes/meta-paths.php'         => '',
+        'src/includes/meta-tables.php'        => '',
+        'src/includes/static-routes.php'      => '',
+        'src/css/styles.css'                  => '',
+        'src/js/lib.js'                       => '',
     ];
 
 
@@ -117,7 +136,8 @@ class Init {
         if ( isset( $_SERVER['REMOTE_ADDR'] ) ) {
             die( 'Permission denied. You may not run this file in the browser' );
         }
-        $this->working_dir = realpath( __DIR__ . '/../../../../' );
+        $this->working_dir   = realpath( __DIR__ . '/../../../../' );
+        $this->template_path = realpath( __DIR__ . '/Init' );
     }
 
 
@@ -158,7 +178,6 @@ class Init {
     public function build(): void {
         $this->build_folder_structure();
         $this->build_file_structure();
-
     }
 
 
@@ -173,12 +192,12 @@ class Init {
 
     public function build_file_structure(): void {
         echo "Creating required files: ";
-        foreach ( self::FILE_STRUCTURE as $file ) {
+        foreach ( self::FILE_STRUCTURE as $file => $template ) {
             $new_file = $this->working_dir . '/' . $file;
             touch( $new_file );
             $write = fopen( $new_file, 'w' );
             fwrite( $write, match( $file ) {
-                'index.php'                           => '',
+                'index.php'                           => $this->get_index( $template ),
                 'src/router.php'                      => '',
                 'src/loader.php'                      => '',
                 'src/app/actions/ActionHandler.php'   => '',
@@ -198,6 +217,30 @@ class Init {
         }
         echo " Done";
         $this->lb();
+    }
+
+
+    /**
+     * Return the php code for the index page.
+     * 
+     * @param   string  $template   The relative path to the template file for index.php.
+     * 
+     * @return  string
+     * 
+     * @access  private
+     * @since   LBF 0.3.0-beta
+     */
+
+    private function get_index( string $template ): string {
+        $data = file_get_contents( $this->template_path . '/' . $template );
+        $data = str_replace( 'START_DATE', date( 'Y-m-d' ), $data );
+        $data = str_replace( 'AUTHOR', $this->data['author'], $data );
+        $version = $this->data['version'];
+        if ( $this->data['status'] !== '' ) {
+            $version .= "-{$this->data['status']}";
+        }
+        $data = str_replace( 'VERSION', $version, $data );
+        return $data;
     }
 
 
