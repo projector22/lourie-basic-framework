a0e79659a926b91433ff7e2717e9e73375382591
Gareth <bravdthepally@gmail.com>
2022-10-10 09:59:08 +0200

    WIP

--- src/init.php
+++ src/init.php
@@ -1,3 +1,4 @@
+#!/usr/bin/env php
 <?php
 
 /**
@@ -13,3 +14,33 @@
  * 
  * @since   LBF 0.0.1
  */
+
+// echo "Enter a name for your app?";
+
+$builder = new Init;
+$builder->gather_data();
+
+
+class Init {
+
+    private array $data;
+
+    public function __construct() {
+        if ( isset( $_SERVER['REMOTE_ADDR'] ) ) {
+            die( 'Permission denied. You may not run this file in the browser' );
+        }
+    }
+
+    public function gather_data() {
+        // $this->data['app_name'] = readline( "Enter a name for your app: " );
+        // $this->data['description'] = readline( "Enter a simple description of your app: " );
+        // $ver = readline( "Enter the starting version, without alpha or beta (Leave blank for '0.1.0'): " );
+        // $this->data['version'] = $ver !== '' ? $ver : '0.1.0';
+        $status = 'z';
+        $valid = ['a', 'b', ''];
+        while ( !in_array( strtolower( $status ), $valid ) ) {
+            $status = readline( "Set the app to an Alpha or Beta status, entry 'a' or 'b' or leave blank to not set to 'live': " );
+        }
+        $this->data['status'] = strtolower( $status );
+    }
+}
