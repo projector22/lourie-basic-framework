632c1cf146c49509ea378d5f8361e20e62acfe4f
Gareth <bravdthepally@gmail.com>
2022-10-20 20:25:11 +0200

    Cleaned up init tool #5

--- src/init.php
+++ src/init.php
@@ -1,25 +1,17 @@
 #!/usr/bin/env php
 <?php
 
-/**
- * @todo    Build this tool
- * 
- * The idea is that, but running it, it builds out the APP environment for you. This is
- * if you are spinning up a new app from scratch. Creating files and folder permissions.
- * 
- * My also be used in deployment, functionally as stage one (or two after `composer install`)
- * of an installer.
- * 
- * It may even be set to hook into the app's local installer.
- * 
- * @since   LBF 0.0.1
- */
-
 $builder = new Init;
-// $builder->gather_data();
+$builder->gather_data();
 $builder->display_data();
 $builder->build();
 
+/**
+ * Class to perform the Init instruction.
+ * 
+ * @author  Gareth Palmer   [Github & Gitlab /projector22]
+ * @since   LBF 0.3.0-beta
+ */
 
 class Init {
 
@@ -32,16 +24,7 @@ class Init {
      * @since   LBF 0.3.0-beta
      */
 
-    // private array $data;
-    private array $data = [
-        'app_name'    => 'Lourie Test App',
-        'description' => 'App for testing the init tool',
-        'author'      => 'Gareth Palmer [Github & Gitlab /projector22]',
-        'version'     => '0.1.0',
-        'status'      => 'alpha',
-        'app-version' => '0.1.0-alpha',
-        'copyright'   => 'Gareth Palmer',
-    ];
+    private array $data;
 
     /**
      * The working directory into which this tool will create all the required files.
