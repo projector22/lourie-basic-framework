2c2897efbaec18cc9f8042f08026ea719302ecc8
Gareth Palmer <bravdthepally@gmail.com>
2022-10-24 09:40:30 +0200

    Added InvalidErrorCode Error

---
+++ src/Errors/General/InvalidErrorCode.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace LBF\Errors\General;
+
+use LBF\Errors\Meta\ExceptionMeta;
+
+/**
+ * If an invalid error code is parsed to an error handler.
+ * 
+ * use LBF\Errors\General\InvalidErrorCode;
+ * 
+ * @author  Gareth Palmer   [Github & Gitlab /projector22]
+ * @since   LBF 0.3.1-beta
+ */
+
+class InvalidErrorCode extends ExceptionMeta {}
