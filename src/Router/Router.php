fe787d16e810e053914cc51a58a08a4b0e541ff7
Gareth <bravdthepally@gmail.com>
2023-02-10 21:51:42 +0200

    Added router class #70

---
+++ src/Router/Router.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace LBF\Router;
+
+use LBF\HTML\HTML;
+
+class Router {
+    public function __construct() {
+
+
+    }
+
+
+    public function route() {
+        $injector = new HTML;
+
+        /**
+         * - [ ] Generate the page data and save it to a variable.
+         * - [ ] Render the <head> tags, with css & js injected.
+         * - [ ] Render the <body> with css & hs injected, then the rendered page data, based on the error code.
+         * - [ ] Render the <footer> followed by the final injected css & js.
+         * - [ ] Close the </body> tag
+         */
+
+    }
+}
