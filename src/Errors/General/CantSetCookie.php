765b6ab1bcb3c8e26e316336261cb36a632c35ed
Gareth <bravdthepally@gmail.com>
2023-02-21 21:51:22 +0200

    Added CantSetCookie #70

---
+++ src/Errors/General/CantSetCookie.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace LBF\Errors\General;
+
+use LBF\Errors\Meta\ExceptionMeta;
+
+/**
+ * If a Cookie cannot be set for whatever reason.
+ * 
+ * use LBF\Errors\General\CantSetCookie;
+ * 
+ * @author  Gareth Palmer   [Github & Gitlab /projector22]
+ * @since   LBF 0.6.0-beta
+ */
+
+class CantSetCookie extends ExceptionMeta {}
