8ba61951dad5ba4f5c775f0cd7e2cc72dc26be55
Gareth <bravdthepally@gmail.com>
2023-02-25 12:40:28 +0200

    Added AppMode enum #70

---
+++ src/Config/AppMode.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace LBF\Config;
+
+/**
+ * Enum defining different modes or states the app may operate under.
+ * 
+ * use LBF\Config\Employment;
+ * 
+ * @author  Gareth Palmer  [Github & Gitlab /projector22]
+ * 
+ * @since   LBF 0.6.0
+ */
+
+enum AppMode: int {
+    case PRODUCTION = 0;
+    case MAINTENANCE = 1;
+    case DEVELOPEMENT = 2;
+}
