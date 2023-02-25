b259e4e684a354d27a6ff1163fe81c3aca896c4c
Gareth <bravdthepally@gmail.com>
2023-02-25 22:22:17 +0200

    Added UserAuth #70

---
+++ src/Auth/UserAuth.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace LBF\Auth;
+
+use Throwable;
+
+class UserAuth {
+
+
+    private array $user_permissions = [
+        'is'  => [],
+        'can' => [],
+    ];
+
+    public function set_permissions( object|array $permissions, bool $is = true ): bool {
+        $is_can = $is ? 'is' : 'can';
+        $this->user_permissions[$is_can] = [];
+        try {
+            foreach( $permissions as $id => $permit ) {
+                $this->user_permissions[$is_can][$permit] = $id;
+            }
+            return true;
+        } catch ( Throwable ) {
+            return false;
+        }
+    }
+
+    public function is( string $permit ): bool {
+        return isset( $this->user_permissions['is'][$permit] );
+    }
+
+    public function can( string $permit ): bool {
+        return isset( $this->user_permissions['can'][$permit] );
+    }
+}
