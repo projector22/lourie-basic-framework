b5c85330c4dec738399da6418b02d737ec62caff
Gareth <bravdthepally@gmail.com>
2023-03-12 13:53:04 +0200

    Added DirectoryNotFound #70

---
+++ src/Errors/Files/DirectoryNotFound.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace LBF\Errors\Files;
+
+use LBF\Errors\Meta\ExceptionMeta;
+
+/**
+ * Error page for handling errors when a file is not found.
+ * 
+ * use LBF\Errors\Files\DirectoryNotFound;
+ * 
+ * @author  Gareth Palmer   [Github & Gitlab /projector22]
+ * @since   LBF 0.6.0
+ */
+
+class DirectoryNotFound extends ExceptionMeta {}
