3ef47d21aa9b25bd9830006af7b765686de7f9b8
Gareth Palmer <bravdthepally@gmail.com>
2023-03-17 21:27:07 +0200

    Updated for #70

--- src/Tools/JSON/RetrieveData.php
+++ src/Tools/JSON/RetrieveData.php
@@ -2,15 +2,44 @@
 
 namespace LBF\Tools\JSON;
 
+/**
+ * Trait to be used with a AppJson enum. Allows for the use of returning data of the specified
+ * enum as an array or object.
+ * 
+ * use LBF\Tools\JSON\RetrieveData;
+ * 
+ * @author  Gareth Palmer  [Github & Gitlab /projector22]
+ * 
+ * @since   LBF 0.6.0-beta
+ */
+
 trait RetrieveData {
 
-    public function data_as_array(): array {
+    /**
+     * Return the contents of the specified JSON file enum as an array.
+     * 
+     * @return  array
+     * 
+     * @access  public
+     * @since   LBF 0.6.0-beta
+     */
+
+    public function as_array(): array {
         return JSONTools::read_json_file_to_array( $this->path() );
     }
 
-    public function data_as_object(): array {
+
+    /**
+     * Return the contents of the specified JSON file enum as an object.
+     * 
+     * @return  object
+     * 
+     * @access  public
+     * @since   LBF 0.6.0-beta
+     */
+
+    public function as_object(): object {
         return JSONTools::read_json_file_to_object( $this->path() );
     }
-
     
 }
