94cef444ab9a3f6e34efb73208a8de9496fcd88b
Gareth <bravdthepally@gmail.com>
2023-03-25 22:57:59 +0200

    Added ClassNotFound

---
+++ src/Errors/Classes/ClassNotFound.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace LBF\Errors\Classes;
+
+use LBF\Errors\Meta\ExceptionMeta;
+
+/**
+ * Error page for handling errors when a called class method does not exist.
+ * 
+ * use LBF\Errors\Classes\ClassNotFound;
+ * 
+ * @author  Gareth Palmer   [Github & Gitlab /projector22]
+ * @since   LBF 0.6.0-beta
+ */
+
+class ClassNotFound extends ExceptionMeta {}
