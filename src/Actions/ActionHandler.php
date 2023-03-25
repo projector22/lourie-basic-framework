69c4f9f35c66b3af2eecd4a48648e4b12f3edd57
Gareth <bravdthepally@gmail.com>
2023-03-25 21:00:52 +0200

    Updated ActionHandler

--- src/Actions/ActionHandler.php
+++ src/Actions/ActionHandler.php
@@ -2,9 +2,30 @@
 
 namespace LBF\Actions;
 
+use LBF\App\Config;
+use LBF\Config\AppMode;
+
 class ActionHandler {
 
+    public string $token;
+
+    public string $routing_class;
+
     public function __construct() {
-        // Any actions
+        $route_token = str_replace( '.php', '', $_POST['route_token'] );
+        if ( $route_token == 'home' ) {
+            $route_token = 'index';
+        }
+        if ( Config::ENVIRONMENT() == AppMode::MAINTENANCE ) {
+            $route_token = 'maintenance';
+        }
+
+        if ( file_exists( Config::paths( 'HOME_PATH' ) . 'FIRST_RUN' ) ) {
+            $route_token = 'FirstRun';
+        }
+
+        $this->routing_class = 'App\\Actions\\Pages\\' . prepare_routed_filename( $route_token ) . 'Actions';
+
+        $this->token = get_token();
     }
 }
