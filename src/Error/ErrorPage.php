b3e19be4695f543dda279c3d464610d766c6d362
Gareth <bravdthepally@gmail.com>
2023-03-26 20:48:20 +0200

    Updated HTTP error pages

--- src/Error/ErrorPage.php
+++ src/Error/ErrorPage.php
@@ -2,6 +2,8 @@
 
 namespace LBF\Error;
 
+use LBF\Assets\HTTPStatusCode;
+
 class ErrorPage {
 
     private static int $code;
@@ -9,21 +11,18 @@ class ErrorPage {
     private static bool $skip_check = false;
 
     public function construct_page() {
-        $file = match( self::$code ) {
-            401     => '401.php',
-            403     => '403.php',
-            404     => '404.php',
-            500     => '500.php',
-            default => '500.php',
+        echo match (self::$code) {
+            401     => $this->error_401(),
+            403     => $this->error_403(),
+            404     => $this->error_404(),
+            500     => $this->error_500(),
+            default => $this->error_500(),
         };
-
-        $path = realpath( __DIR__ . '/../Error/' . $file );
-        require $path;
     }
 
 
 
-    public static function set_error( int $code, $skip_check = false ): void {
+    public static function set_error(int $code, $skip_check = false): void {
         self::$code = $code;
         self::$skip_check = $skip_check;
     }
@@ -34,5 +33,81 @@ class ErrorPage {
 
     public static function skip_check(): bool {
         return self::$skip_check;
+    }
+
+
+    /**
+     * Draw out the page that will display when there is a 401 error.
+     * 
+     * @access  private
+     * @since   LRS 3.9.1   Reworked and remade
+     * @since   LRS 3.28.0  Seperated out of `Lourie Registration System` into `Lourie Basic Framework`.
+     *                      Namespace changed from `Framework` to `LBF`.
+     * @since   LBF 0.6.0   Merged into ErrorPage Class.
+     */
+
+    private function error_401(): void {
+        $image = html_path(HTTPStatusCode::UNAUTHORIZED->image()->path());
+        echo "<div class='container__401'>";
+        echo "<h3>Can't find your creds here mate...</h3>";
+        echo "<img class='error_img' src='{$image}'>";
+        echo "<h1>401 - Unauthorized</h1>";
+        echo "</div>";
+    }
+
+
+    /**
+     * Draw out the page that will display when there is a 403 error.
+     * 
+     * @access  private
+     * @since   LRS 3.9.1   Reworked and remade
+     * @since   LRS 3.28.0  Seperated out of `Lourie Registration System` into `Lourie Basic Framework`.
+     *                      Namespace changed from `Framework` to `LBF`.
+     * @since   LBF 0.6.0   Merged into ErrorPage Class.
+     */
+
+    private function error_403(): void {
+        $image = html_path(HTTPStatusCode::FORBIDDEN->image()->path());
+        echo "<div class='container__403'>";
+        echo "<h3>You shall not pass!</h3>";
+        echo "<img class='error_img' src='{$image}'>";
+        echo "<h1>403 - Forbidden</h1>";
+        echo "</div>";
+    }
+
+
+    /**
+     * Draw out the page that will display when there is a 404 error.
+     * 
+     * @access  private
+     * @since   LRS 3.1.0
+     * @since   LRS 3.9.1   Reworked and remade, new image & text
+     * @since   LRS 3.28.0  Seperated out of `Lourie Registration System` into `Lourie Basic Framework`.
+     *                      Namespace changed from `Framework` to `LBF`.
+     * @since   LBF 0.6.0   Merged into ErrorPage Class.
+     */
+
+    private function error_404(): void {
+
+        $image = html_path(HTTPStatusCode::NOT_FOUND->image()->path());
+        echo "<div class='container__404'>";
+        echo "<h3>Um... yeah</h3>";
+        echo "<img class='error_img' src='{$image}'>";
+        echo "<h1>404 - Page Not Found</h1>";
+        echo "</div>";
+    }
+
+
+    /**
+     * Draw out the 500 error page.
+     * 
+     * @access  private
+     * @since   LBF 0.6.0
+     */
+
+    private function error_500(): void {
+        echo "<h2>Error 500</h2>";
+        echo "<h3>Internal Server Error</h3>";
+        echo "The server has encountered a situation it does not know how to handle.";
     }
 }
