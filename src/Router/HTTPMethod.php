c23fe0433db561513bda0512cf02d8e3aaac2391
Gareth <bravdthepally@gmail.com>
2023-04-08 19:00:39 +0200

    Added comments #70

--- src/Router/HTTPMethod.php
+++ src/Router/HTTPMethod.php
@@ -2,6 +2,16 @@
 
 namespace LBF\Router;
 
+/**
+ * Enum representing the various HTTP request methods possible.
+ * 
+ * use LBF\Router\HTTPMethod;
+ * 
+ * @author  Gareth Palmer  [Github & Gitlab /projector22]
+ * 
+ * @since   4.0.0
+ */
+
 enum HTTPMethod {
     case GET;
     case POST;
