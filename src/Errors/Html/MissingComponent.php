dadb072a7d3f28dcc19cbfc6d5bb5466663fa5bf
Gareth Palmer <bravdthepally@gmail.com>
2023-04-15 21:49:18 +0200

    Added exception MissingComponent

---
+++ src/Errors/Html/MissingComponent.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace LBF\Errors\MissingComponent;
+
+use LBF\Errors\Meta\ExceptionMeta;
+
+/**
+ * If a part of a class is missing, this is fired.
+ * 
+ * use LBF\Errors\MissingComponent\MissingComponent;
+ * 
+ * @author  Gareth Palmer   [Github & Gitlab /projector22]
+ * @since   LBF 0.7.0-beta
+ */
+
+class MissingComponent extends ExceptionMeta {
+}
