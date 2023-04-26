0d07434decad6ba957f1f78aa0d838242373ad05
Gareth <bravdthepally@gmail.com>
2023-04-26 20:46:04 +0200

    Added object recognition #76

---
+++ src/Errors/Array/PropertyNotInObject.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace LBF\Errors\Array;
+
+use LBF\Errors\Meta\ExceptionMeta;
+
+/**
+ * Error page for handling errors when a called class method does not exist.
+ * 
+ * use LBF\Errors\Classes\PropertyNotInObject;
+ * 
+ * @author  Gareth Palmer   [Github & Gitlab /projector22]
+ * @since   LBF 0.7.0-beta
+ */
+
+class PropertyNotInObject extends ExceptionMeta {
+}
--- src/Tools/Array/ArrayTool.php
+++ src/Tools/Array/ArrayTool.php
@@ -3,23 +3,40 @@
 namespace LBF\Tools\Array;
 
 use LBF\Errors\Array\IndexNotInArray;
+use LBF\Errors\Array\PropertyNotInObject;
+use LBF\Errors\Array\ScalarVariable;
 
 class ArrayTool {
 
     /**
      * @throws  IndexNotInArray
+     * @throws  PropertyNotInObject
+     * @throws  ScalarVariable
      */
 
     public static function index_by(string $index, array $array, bool $remove_value = false): array {
         $new_array = [];
-        foreach ($array as $subarray) {
-            if (!isset($subarray[$index])) {
-                throw new IndexNotInArray("'{$index}' not in a subarray of your array");
+        foreach ($array as $subvalue) {
+            if (is_array($subvalue)) {
+                if (!isset($subvalue[$index])) {
+                    throw new IndexNotInArray("'{$index}' not in a subarray of your array");
+                }
+                $new_array[$subvalue[$index]] = $subvalue;
+                if ($remove_value) {
+                    unset($subvalue[$index]);
+                }
+            } else if (is_object($subvalue)) {
+                if (!isset($subvalue->$index)) {
+                    throw new PropertyNotInObject("'{$index}' not in a subobject of your array");
+                }
+                $new_array[$subvalue->$index] = $subvalue;
+                if ($remove_value) {
+                    unset($subvalue->$index);
+                }
+            } else {
+                throw new ScalarVariable("The subvalue is scalar, and does not have a property, field or index");
             }
-            if ($remove_value) {
-                unset($subarray[$index]);
-            }
-            $new_array[$subarray[$index]] = $subarray;
         }
+        return $new_array;
     }
 }
