3345d3806c9a88eaf0262ce7590f8b8e1c0054de
Gareth Palmer <bravdthepally@gmail.com>
2023-05-21 21:07:41 +0200

    Added Disabled #80

---
+++ src/Errors/LDAP/LDAPDisabled.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace LBF\Errors\IO;
+
+use LBF\Errors\Meta\ExceptionMeta;
+
+/**
+ * Error page for handling errors that occures when an LDAP connection fails.
+ * 
+ * use LBF\Errors\LDAP\LDAPConnect;
+ * 
+ * @author  Gareth Palmer   [Github & Gitlab /projector22]
+ * @since   LBF 0.8.0-beta
+ */
+
+class LDAPDisabled extends ExceptionMeta {
+}
