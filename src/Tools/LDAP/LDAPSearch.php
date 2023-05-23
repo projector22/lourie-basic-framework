ba93064ab7f912507d5cb4e54d159ad37dd382e9
Gareth <bravdthepally@gmail.com>
2023-05-23 21:39:31 +0200

    Updated search #80

--- src/Tools/LDAP/LDAPSearch.php
+++ src/Tools/LDAP/LDAPSearch.php
@@ -6,7 +6,6 @@ use LBF\Tools\LDAP\Base\BaseLDAP;
 
 final class LDAPSearch extends BaseLDAP {
 
-
     /**
      * Pull entries out of an Active Directory via LDAP into an array
      * 
@@ -19,6 +18,8 @@ final class LDAPSearch extends BaseLDAP {
      * 
      * @access  public
      * @since   LRS 3.1.0
+     * 
+     * @deprecated  LBF 0.8.0-beta
      */
 
     public function ldap_search(string $object_category = 'user', string $sam_account_name = '*', ?string $search_ou = null): array|bool {
@@ -35,42 +36,87 @@ final class LDAPSearch extends BaseLDAP {
 
 
     /**
+     * Perform a search, by parsing the OU and filter string.
+     * 
+     * @param   string  $ou     The OU in which to search.
+     * @param   string  $filter The filer string with which to seach. Examples:
+     *                          - `(&(objectClass=group)(distinguishedName=myGroup))`
+     *                          - `(&(objectCategory=user)(samaccountname=myUserName))`
+     * 
+     * @return  array|false
+     * 
+     * @see https://wiki.mozilla.org/Mozilla_LDAP_SDK_Programmer%27s_Guide/Searching_the_Directory_With_LDAP_C_SDK
+     * @see http://www.faqs.org/rfcs/rfc4515.html
+     * 
+     * @access  public
+     * @since   LBF 0.8.0-beta
+     */
+    public function search(string $ou, string $filter): array|false {
+        $results = ldap_search($this->ldap->conn, $ou, $filter);
+        return ldap_get_entries($this->ldap->conn, $results);
+    }
+
+
+    /**
+     * Find the user specified or all users within a nominated OU.
+     * 
+     * @param   string  $ou     The OU in which to search.
+     * @param   string  $sam_account_name   The LDAP attribute `sAMAccountName` 
+     *                                      which is tied to a user account. 
+     *                                      Default '*' which searches for all users within the ou.
+     * 
+     * @return  array|false
+     * 
+     * @access  public
+     * @since   LBF 0.8.0-beta
+     */
+
+    public function find_users(string $ou, string $sam_account_name = '*'): array|false {
+        if ($this->ldap->bind()) {
+            $filter = "(&(objectCategory=user)(samaccountname={$sam_account_name}))";
+            return $this->search($ou, $filter);
+        }
+        return false;
+    }
+
+    /**
+     * @todo build this
+     */
+
+    // public function find_groups() {};
+
+    /**
+     * @todo build this
+     */
+
+    // public function find_ous() {};
+
+
+    /**
      * Get all the members of a group
      * 
      * @param   string  $group_dn   Set a custom or alternative group_dn to the teacher or student context
      * 
-     * @return array|bool  False if failed
+     * @return array|false
      * 
      * @access  public
      * @since   LRS 3.11.0
      * @since   LRS 3.12.1  Added recursiveness to get members of groups within the defined group
+     * @since   LBF 0.8.0-beta  Renamed from `get_group_members` to `find_group_members`
      */
 
-    public function get_group_members(string $group_dn): array|bool {
-        $get_group_location = function ($context) {
-            // Search through the whole of the Domain Controller
-            $hold = array_reverse(explode(',', $context));
-            foreach ($hold as $i => $item) {
-                if (substr($item, 0, 2) !== 'DC') {
-                    unset($hold[$i]);
-                }
-            }
-            return implode(',', array_reverse($hold));
-        };
+    public function find_group_members(string $group_dn): array|false {
         if ($this->ldap->bind()) {
-            $context = $group_dn;
-            $group_location = $get_group_location($context);
+            $filter = "(memberof={$group_dn})";
+            $entries = $this->search($this->get_group_context_location($group_dn), $filter);
 
-            $filter = "(memberof={$context})";
-            $results = ldap_search($this->ldap->conn, $group_location, $filter);
-            $entries = ldap_get_entries($this->ldap->conn, $results);
             // $context_hold = $this->context;
             $new_members = [];
             foreach ($entries as $index => $entry) {
                 if (isset($entry['objectclass'][1]) && $entry['objectclass'][1] == 'group') {
                     // $this->context = 'self';
                     $dn = $entry['distinguishedname'][0];
-                    $new_members[] = $this->get_group_members($dn);
+                    $new_members[] = $this->find_group_members($dn);
                     unset($entries[$index]);
                 }
             }
@@ -89,6 +135,25 @@ final class LDAPSearch extends BaseLDAP {
 
 
     /**
+     * Get all the members of a group
+     * 
+     * @param   string  $group_dn   Set a custom or alternative group_dn to the teacher or student context
+     * 
+     * @return array|false
+     * 
+     * @access  public
+     * @since   LRS 3.11.0
+     * @since   LRS 3.12.1  Added recursiveness to get members of groups within the defined group
+     * 
+     * @deprecated  LBF 0.8.0-beta -> RENAME `find_group_members`
+     */
+
+    public function get_group_members(string $group_dn): array|false {
+        return $this->find_group_members($group_dn);
+    }
+
+
+    /**
      * Get all the users of an OU
      * 
      * @param   string  $ou_dn   Set a custom or alternative ou_dn to the teacher or student context.
@@ -97,31 +162,23 @@ final class LDAPSearch extends BaseLDAP {
      * 
      * @access  public
      * @since   LRS 3.11.0
+     * 
+     * @deprecated  LBF 0.8.0 -> USE `find_users` instead.
      */
 
     public function get_ou_user_members(string $location): array|false {
-        if ($this->ldap->bind()) {
-            $filter = "(&(objectCategory=user)(samaccountname=*))";
-            $results = ldap_search($this->ldap->conn, $location, $filter);
-            return ldap_get_entries($this->ldap->conn, $results);
-        }
-        return false;
+        return $this->find_users($location);
     }
 
-
     /**
-     * Check if a specified group exists on the AD server
-     * 
-     * @param   string  $group  The DN of the group to be tests
-     * 
-     * @return  boolean
-     * 
-     * @access  public
-     * @since   LRS 3.11.0
+     * @todo build this
      */
 
-    public function test_group_exists(string $group): bool {
-        if ($group == '') {
+
+    // public function user_exists(string $user): bool {}
+
+    public function group_exists(string $group): bool {
+        if (empty($group)) {
             return false;
         }
         if ($this->ldap->bind()) {
@@ -141,6 +198,24 @@ final class LDAPSearch extends BaseLDAP {
     }
 
 
+    /**
+     * Check if a specified group exists on the AD server
+     * 
+     * @param   string  $group  The DN of the group to be tests
+     * 
+     * @return  boolean
+     * 
+     * @access  public
+     * @since   LRS 3.11.0
+     * 
+     * @deprecated  LBF 0.8.0 -> USE `group_exists` instead.
+     */
+
+    public function test_group_exists(string $group): bool {
+        return $this->group_exists($group);
+    }
+
+
     private function get_group_location(string $group): string {
         $hold = array_reverse(explode(',', $group));
         $skip = false;
@@ -151,6 +226,18 @@ final class LDAPSearch extends BaseLDAP {
             }
             if (substr($item, 0, 2) == 'OU') {
                 $skip = true;
+            }
+        }
+        return implode(',', array_reverse($hold));
+    }
+
+
+    private function get_group_context_location(string $context): string {
+        // Search through the whole of the Domain Controller
+        $hold = array_reverse(explode(',', $context));
+        foreach ($hold as $i => $item) {
+            if (substr($item, 0, 2) !== 'DC') {
+                unset($hold[$i]);
             }
         }
         return implode(',', array_reverse($hold));
